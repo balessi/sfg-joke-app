6326c6d92dc84767e1ff2d716f1983f6b89b9cb4
x215617 <X215617@prservicos.com.br>
2020-07-21 16:44:01 -0300

    first commit

---
+++ src/test/java/guru/springframework/sfgjokeapp/SfgJokeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgjokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJokeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
